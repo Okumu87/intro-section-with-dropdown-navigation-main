3dc1dcc0337c7ce6d58fbf2b974ab4873469e854
okumu87 <okumu793@gmail.com>
2025-07-04 16:19:10 +0200

    desktop start

--- app.js
+++ app.js
@@ -22,3 +22,38 @@ document.getElementById('close-btn').addEventListener('click', function() {
 
 
 //collapse menus and down arrow 
+
+const dropdownFeatures = document.getElementById('arrow-down-features');
+
+dropdownFeatures.addEventListener('click', function() {
+    const dropdownContent = document.querySelector('.dropdown-features');
+    const arrowIcon = dropdownFeatures.querySelector('img');
+
+    // Toggle visibility of dropdown content
+    dropdownContent.classList.toggle('hidden');
+
+    // Change arrow icon based on visibility
+    if (dropdownContent.classList.contains('hidden')) {
+        arrowIcon.src = ARROW_DOWN_IMG_PATH;
+    } else {
+        arrowIcon.src = ARROW_UP_IMG_PATH;
+    }
+});
+// company dropdown
+const dropdownCompany = document.getElementById('arrow-down-company');
+
+dropdownCompany.addEventListener('click', function() {
+    const dropdownContent = document.querySelector('.dropdown-company');
+    const arrowIcon = dropdownCompany.querySelector('img');
+
+    // Toggle visibility of dropdown content
+    dropdownContent.classList.toggle('hidden');
+
+    // Change arrow icon based on visibility
+    if (dropdownContent.classList.contains('hidden')) {
+        arrowIcon.src = ARROW_DOWN_IMG_PATH;
+    } else {
+        arrowIcon.src = ARROW_UP_IMG_PATH;
+    }
+});
+
