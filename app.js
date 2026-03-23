99172892395da95fe63afb70b9ba91880bec1681
okumu87 <okumu793@gmail.com>
2026-03-23 17:57:28 +0100

    mobile menu button and overlay

--- app.js
+++ app.js
@@ -16,3 +16,22 @@ function initNavigation() {
 
 // Ensure the DOM is fully loaded before running
 document.addEventListener("DOMContentLoaded", initNavigation);
+
+// mobile menu toggle
+const hamburgerMenuBtn = document.querySelector(".hamburger-menu-btn");
+const menuIcon = hamburgerMenuBtn.querySelector("img");
+hamburgerMenuBtn.addEventListener("click", () => {
+    // Check if the menu is currently open (you can use a class on the nav or a data-attribute)
+    const isOpened = navMenu.classList.contains("open");
+
+    if (isOpened) {
+        // Change back to Hamburger icon
+        menuIcon.src = "./images/icon-menu.svg";
+    } else {
+        // Change to Close (X) icon
+        menuIcon.src = "./images/icon-close-menu.svg";
+    }
+    
+    // Don't forget to actually toggle your menu!
+    navMenu.classList.toggle("open");
+});
