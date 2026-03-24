50e08971b3130d132648de13ae9fc33f26928f6c
okumu87 <okumu793@gmail.com>
2026-03-24 18:23:24 +0100

    working on mobile menu

--- app.js
+++ app.js
@@ -17,21 +17,11 @@ function initNavigation() {
 // Ensure the DOM is fully loaded before running
 document.addEventListener("DOMContentLoaded", initNavigation);
 
-// mobile menu toggle
-const hamburgerMenuBtn = document.querySelector(".hamburger-menu-btn");
-const menuIcon = hamburgerMenuBtn.querySelector("img");
-hamburgerMenuBtn.addEventListener("click", () => {
-    // Check if the menu is currently open (you can use a class on the nav or a data-attribute)
-    const isOpened = navMenu.classList.contains("open");
+const hamburgerMenu = document.querySelector(".hamburger-menu-btn");
+const navMenu = document.querySelector(".nav-links");
 
-    if (isOpened) {
-        // Change back to Hamburger icon
-        menuIcon.src = "./images/icon-menu.svg";
-    } else {
-        // Change to Close (X) icon
-        menuIcon.src = "./images/icon-close-menu.svg";
-    }
-    
-    // Don't forget to actually toggle your menu!
-    navMenu.classList.toggle("open");
+hamburgerMenu.addEventListener("click", () => {
+  console.log("Hamburger menu clicked");
+  console.log(navMenu);
+  navMenu.classList.toggle("active");
 });
